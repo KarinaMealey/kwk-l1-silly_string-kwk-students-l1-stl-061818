91ee5e632d4b845e4aa2e28aa1af928518d4155a
KarinaMealey <kem32002@gmail.com>
2018-06-19 14:17:31 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,21 +1,21 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "karina"
 
 puts "My favorite word is #{your_favorite_word}"
 
-# Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
+# Challenge 2: Capitalize every letter in karina using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = # Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "my favorite word is #{your_favorite_word.upcase}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "IM NOT SHOUTING"
+phrase = "IM NOT SHOUTING".swapcase
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = "im not shouting"
 
 puts lowercased_phrase
 
@@ -23,7 +23,7 @@ puts lowercased_phrase
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word # Put a .length after big_word and use a String method.
 
 puts "There are #{letter_count} in #{big_word}"
 
@@ -37,12 +37,12 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".Capitalize # Put a .capitalize after the string to capitalize it properly.
 
 puts proper_sentence
 
 # Challenge 7: String Switcheroo
-sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub"a".gsub"o"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
